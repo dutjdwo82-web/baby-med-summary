e70fbbfdde5c76b4ab90181773316c287ccea68c
dutjdwo82-web <dutjdwo82@gmail.com>
2026-09-09 17:40:28 +0900

    모바일 최적화, 아프지마 폰트/사이즈 변경, 테두리 두껍게, 음영 제거

--- app.py
+++ app.py
@@ -92,6 +92,7 @@ html, body {
     text-align: center;
     font-size: 7rem;
     line-height: 1;
+    margin-top: 30px;
     margin-bottom: 0px;
     filter: drop-shadow(0 4px 6px rgba(0,0,0,0.2));
 }
